4a973ea4ccc94b38f4dd00f615d5fffe24d85a11
lagofelipe <lagofelipe@gmail.com>
2020-10-07 21:06:32 -0500

    done!

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+attr_accessor :name
+
+@@all = []
+def initialize(name)
+    @name = name
+
+self.save
+end
+
+def save
+@@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.clear_all
+@@all.clear
+end
+
+def self.print_all
+    #interate thru array priting each evement 
+printout = self.all.collect {|dog| dog.name}
+puts printout
+end
+
+####
+end#####
